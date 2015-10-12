8083e3682f424f505b1ddb2af344b461fe7c24ae
Johan Talboom <borfje@gmail.com>
2015-10-12 20:30:12 +0200

    Split up jsonserver

---
+++ JsonServer.js
@@ -0,0 +1,63 @@
+﻿net = require('net');
+var clients = [];
+
+var callbacks = {};
+
+exports.bind = function (name, callback) {
+	callbacks[name] = callback;	
+}
+
+
+exports.start = function (port) {
+	net.createServer(function (socket) {
+		socket.name = socket.remoteAddress + ":" + socket.remotePort
+		socket.buffer = "";
+		socket.setEncoding("binary");
+		clients.push(socket);
+		console.log(socket.name + " Connected.\n");
+		
+		socket.on('data', function (data) {
+			socket.buffer += data;
+			while (socket.buffer.length >= 4) {
+				var len = new Buffer(socket.buffer, "binary").readUInt32LE(0);
+				if (socket.buffer.length >= 4 + len) {
+					var packet = JSON.parse(socket.buffer.slice(4, len + 4));
+					if (packet.hasOwnProperty("id") && callbacks.hasOwnProperty(packet.id))
+						callbacks[packet.id](packet, null);
+					else {
+						console.log("Got invalid packet");
+						if (packet.hasOwnProperty("id")) {
+							console.log("Packet: " + packet.id);
+							console.log(packet);
+						}
+						socket.end();
+						socket.destroy();
+					}
+					socket.buffer = socket.buffer.slice(4 + len);
+				} else
+					break;
+			}
+		});
+		socket.on('end', function () {
+			clients.splice(clients.indexOf(socket), 1);
+			console.log(socket.name + " Disconnected.\n");
+		});
+		socket.on('close', function () {
+			clients.splice(clients.indexOf(socket), 1);
+			console.log(socket.name + " Closed.\n");
+		});
+		socket.on('error', function () {
+			clients.splice(clients.indexOf(socket), 1);
+			console.log(socket.name + " Errored.\n");
+		});
+		
+		function broadcast(message, sender) {
+			clients.forEach(function (client) {
+				if (client === sender) return;
+				client.write(message);
+			});
+			process.stdout.write(message);
+		}
+
+	}).listen(port);
+}
--- app.js
+++ app.js
@@ -1,40 +1,9 @@
-﻿net = require('net');
-var clients = [];
+﻿jsonServer = require("./JsonServer.js")
 
-net.createServer(function (socket) {
-	socket.name = socket.remoteAddress + ":" + socket.remotePort
-	socket.buffer = "";
-	clients.push(socket);
-	console.log(socket.name + " Connected.\n");
-	
-	socket.on('data', function (data) {
-		socket.buffer += data;
-		while(socket.buffer.length >= 4) {
-			var len = new Buffer(socket.buffer, 4).readUInt32LE(0);
-			if (socket.buffer.length >= 4 + len) {
-				var packet = JSON.parse(socket.buffer.slice(4, len + 4));
-				console.log(packet);
-				socket.buffer = socket.buffer.slice(4 + len);
-			} else
-				break;
 
-		}
+jsonServer.bind('session/start', function (req, res) {
+	console.log(req);
+});
 
-	});
-	socket.on('end', function () {
-		clients.splice(clients.indexOf(socket), 1);
-		console.log(socket.name + " Disconnected.\n");
-	});
-	
-	function broadcast(message, sender) {
-		clients.forEach(function (client) {
-			if (client === sender) return;
-			client.write(message);
-		});
-		process.stdout.write(message);
-	}
-
-}).listen(5000);
-
-console.log('Node JS Version: ' + process.version);
+jsonServer.start(5000);
 console.log("server running at port 5000\n");
