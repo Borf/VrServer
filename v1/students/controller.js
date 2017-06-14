37c93856cd6c5e63b506c74375f0af34fee63332
Jannick van Ballegooijen <jannickvb@msn.com>
2017-06-14 15:43:21 +0200

    add error handlers, add test function

--- v1/students/controller.js
+++ v1/students/controller.js
@@ -1,5 +1,22 @@
+const _ = require('lodash');
+const ErrorHandler = require('../../lib/error.handler');
+const SuccessHandler = require('../../lib/success.handler');
+const RabbitHandler = require('../../lib/rabbit.handler');
+const Student = require('./model')
+
 exports.findAll = function (req, res) {
-    res.send('not implemented yet');
+    Student
+        .find()
+        .sort('-name')
+        .lean()
+        .then(function(students){
+            return students;
+        })
+        .then(function(students) {
+            SuccessHandler.handle(res, 200, warnings);
+        }, function(error) {
+            ErrorHandler.handle(res, error, 404);
+        });
 }
 
 exports.findByStudentNr = function(req, res) {
